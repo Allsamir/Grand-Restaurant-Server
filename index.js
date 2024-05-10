f55e5ef7fb1ddff48d2e46e5679add81dafab1f5
Al Samir <shahinur1591@gmail.com>
2024-05-11 00:48:46 +0600

    Database connected

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const api = require("./apis/api");
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.error(err));
+
 app.use(api);
+
 app.get("/", (req, res) => {
   res.send("Hello From Grand Resturant");
 });
-
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
